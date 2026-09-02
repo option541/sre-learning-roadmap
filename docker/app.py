2417afd9b243f072dbf937c3730098e7ac975bc1
option541 <347807416@qq.com>
2026-09-02 23:58:49 +0800

    Docker

--- docker/app.py
+++ docker/app.py
@@ -1,19 +1,29 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from datetime import datetime
+import os
 
 
 class HealthHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         if self.path == "/health":
-            self.send_response(200)
+            self.send_response(500)
             self.send_header("Content-Type", "text/plain")
             self.end_headers()
 
             now = datetime.now()
             self.wfile.write(
-                f"{now} SRE demo service healthy\n".encode()
+                f"{now} SRE demo service unhealthy\n".encode()
             )
+
+        elif self.path == "/crash":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b"Service will crash now\n")
+            self.wfile.flush()
+
+            os._exit(1)
+
         else:
             self.send_response(404)
             self.end_headers()
